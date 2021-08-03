0bbd47b7f4b42898b03b5f13e4697fc67681db43
diya <diya17071999@gmail.com>
2021-08-03 16:12:50 +0530

    random hex code generation

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ const buttons = document.getElementsByClassName("hexcode");
 const colors = document.getElementsByClassName("color");
 //console.log(colors); all the colors
 
-
+/*for(let i=0;i<buttons.length;i++){
+	let button = buttons[i];
+	let color = colors[i];
+	
+	button.addEventListener("click", function() {
+	  let newColor = getRandomHex();
+	  color.style.backgroundColor = newColor;
+	  color.style.color =newColor;
+ 	  button.textContent = newColor;
+	});
+	
+	color.addEventListener("click", function() {
+		button.innerHTML;
+  		document.execCommand("copy");
+  		alert("Copied!");
+  	});
+}*/
 
 
 function getRandomNumber() {
