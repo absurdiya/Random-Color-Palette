a7e582c260b4a27925f4c730463797c17c4f0823
diya <diya17071999@gmail.com>
2021-08-03 16:16:02 +0530

    corrected event listening and added snackbar

--- app.js
+++ app.js
@@ -16,9 +16,20 @@ for(let i=0;i<buttons.length;i++){
 	});
 	
 	color.addEventListener("click", function() {
-		button.innerHTML;
-  		document.execCommand("copy");
-  		alert("Copied!");
+	
+		//this does not work to copy to clipboard
+		//write to the clipboard
+		
+		//this does though, we can use select and execCommand copy only when we have an input field
+		let tocopy = button.textContent;
+		copyStringToClipboard(tocopy);
+  		//alert("Copied!");
+  		
+ 
+  		//copied! signal
+  		let x = document.getElementById("snackbar");
+  		x.style.visibility = "visible";
+  		setTimeout(function(){x.style.visibility = "hidden";},1000)
   	});
 }
 
@@ -33,4 +44,22 @@ function getRandomHex() {
   	}
   	return hexColor;
 }
+function copyStringToClipboard (str) {
+   // Create new element
+   var el = document.createElement('textarea');
+   // Set value (string to be copied)
+   el.value = str;
+   // Set non-editable to avoid focus and move outside of view
+   el.setAttribute('readonly', '');
+   el.style = {position: 'absolute', left: '-9999px'};
+   document.body.appendChild(el);
+   // Select text inside element
+   el.select();
+   // Copy text to clipboard
+   document.execCommand('copy');
+   // Remove temporary element
+   document.body.removeChild(el);
+}
 
+
+//CAUTION: Script added at the end of HTML document or it will run before the oage loads and I'll get errors(null)
